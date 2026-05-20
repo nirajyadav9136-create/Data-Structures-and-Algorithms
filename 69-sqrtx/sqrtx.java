03766c2e2b79bfd10515ae097c365843cd7fffb0
nirajyadav9136-create <nirajyadav9136@gmail.com>
2026-05-20 23:46:26 +0530

    Time: 1 ms (97.98%) | Memory: 42.7 MB (22.69%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int mySqrt(int x) {
+        int start = 0;
+        int end = x ;
+        int ans = 0;
+        
+        while(start <= end){
+            int mid = start +(end - start)/2;
+            long square = (long) mid * mid ;
+            if(square == x){
+                ans = mid ;
+                return mid ;
+            } 
+            if(square < x ){
+                ans = mid ;
+                start = mid + 1;
+            }
+            else{
+                end = mid - 1;
+            }
+        }
+        return ans ;
+    }
+}
